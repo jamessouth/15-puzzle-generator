d7b2f78bfcc370b9126a4645fbdd66557c4bdf06
jamessouth <jamesdanielsouth@gmail.com>
2019-03-19 03:11:30 -0500

    add images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FormComponent } from './home/form/form.component';
+import { LanguageSelectComponent } from './home/language-select/language-select.component';
+import { CodeComponent } from './home/code/code.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FormComponent,
+    LanguageSelectComponent,
+    CodeComponent,
+    FooterComponent,
+    DemoComponent,
   ],
   imports: [
     BrowserModule,
