c5b33ac083ddbbe9d3984b8ec942f38f32a95bbc
jamessouth <jamesdanielsouth@gmail.com>
2019-04-09 19:14:02 -0500

    finish demo

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -16,33 +16,19 @@ export class DemoComponent implements OnInit {
   boardOrder: Array<number>;
   drawOrder: Array<number>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
 
 
-    // let gameAlreadyWon;
-    // if(sessionStorage.getItem('gameOver')){
-    //   try{
-    //     gameAlreadyWon = sessionStorage.getItem('gameOver');
-    //   } catch(e) {
-    //     sessionStorage.removeItem('gameOver');
-    //   }
-    // }
-
-    // if (gameAlreadyWon) {
-    //   this.gameOver = true;
-    //   this.pic.onload = () => {
-    //     this.ctx.drawImage(this.pic, 0, 0);
-    //   }
-    //   this.pic.src = "../../assets/images/mucha.jpg";
-    // } else {
 
     this.canvArray = getCanvArray(7, 5);
     this.canvas = document.querySelector('canvas');
     this.ctx = this.canvas.getContext('2d');
+
+
     let game;
-    if(sessionStorage.getItem('boardOrder')){
+    if (sessionStorage.getItem('boardOrder')) {
       try{
         game = JSON.parse(sessionStorage.getItem('boardOrder'));
       } catch(e){
@@ -56,12 +42,17 @@ export class DemoComponent implements OnInit {
       game.forEach((x, i) => {
         drawOrder[x] = i;
       });
-      this.useCanvas(drawOrder);
+      if (this.checkForWin(game)) {
+        this.useCanvas(drawOrder, 0);
+        this.gameOver = true;
+      } else {
+        this.useCanvas(drawOrder, 1);
+      }
     } else {
 
 
       [this.boardOrder, this.drawOrder] = getBoardOrder(35);
-      this.useCanvas(this.drawOrder);
+      this.useCanvas(this.drawOrder, 1);
     }
 
 
@@ -73,17 +64,10 @@ export class DemoComponent implements OnInit {
 
   }
 
-
-
   saveGame(arr): void {
     const savedGame = JSON.stringify(arr);
     sessionStorage.setItem('boardOrder', savedGame);
   }
-
-  // getSavedGame() {
-  //
-  //     return game;
-  //   }
 
 
   resetGame() {
@@ -92,7 +76,7 @@ export class DemoComponent implements OnInit {
     this.ctx.clearRect(0, 0, 410, 574);
 
     [this.boardOrder, this.drawOrder] = getBoardOrder(35);
-    this.useCanvas(this.drawOrder);
+    this.useCanvas(this.drawOrder, 1);
   }
 
 
@@ -114,32 +98,44 @@ export class DemoComponent implements OnInit {
         return;
       }
     }
-    let finalCheck;
+    let interimCheck;
     let brdInd = this.boardOrder[tileClicked];
     this.ctx.clearRect(this.canvArray[tileClicked][0], this.canvArray[tileClicked][1], 82, 82);
     this.ctx.drawImage(this.pic, this.canvArray[brdInd][0], this.canvArray[brdInd][1], 82, 82, this.canvArray[blank][0], this.canvArray[blank][1], 82, 82);
     [this.boardOrder[tileClicked], this.boardOrder[blank]] = [this.boardOrder[blank], this.boardOrder[tileClicked]];
-    if(this.boardOrder[0] === 0 && this.boardOrder[4] === 4 && this.boardOrder[29] === 29 && this.boardOrder[33] === 33){
-      finalCheck = true;
-      for(let f = 0; f < this.boardOrder.length; f++){
-        if(this.boardOrder[f] !== f){
-          finalCheck = false;
-          break;
-        }
-      }
+
+
+
+    if (this.boardOrder[0] === 0 && this.boardOrder[4] === 4 && this.boardOrder[29] === 29 && this.boardOrder[33] === 33) {
+      interimCheck = this.checkForWin(this.boardOrder);
     }
-    if(finalCheck){
+
+
+    if (interimCheck) {
       this.ctx.drawImage(this.pic, 328, 492, 82, 82, 328, 492, 82, 82);
       this.gameOver = true;
     }
+
   }
 
 
 
+  checkForWin(arr) {
+    let finalCheck = true;
+    for (let f = 0; f < arr.length; f++) {
+      if (arr[f] !== f) {
+        finalCheck = false;
+        break;
+      }
+    }
+    return finalCheck;
+  }
 
-  useCanvas(arr) {
+
+
+  useCanvas(arr, gameFinished) {
     this.pic.onload = () => {
-      for(let i = 0; i < this.canvArray.length - 1; i++){
+      for(let i = 0; i < this.canvArray.length - gameFinished; i++){
         this.ctx.drawImage(this.pic, this.canvArray[i][0], this.canvArray[i][1], 82, 82, this.canvArray[arr[i]][0], this.canvArray[arr[i]][1], 82, 82);
       }
     }
