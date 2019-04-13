b4c4e64837e4fc008ac39c91156cd5c6043876e6
jamessouth <jamesdanielsouth@gmail.com>
2019-04-13 17:32:13 -0500

    fix paths

--- src/app/artwork/artwork.component.ts
+++ src/app/artwork/artwork.component.ts
@@ -9,49 +9,49 @@ export class ArtworkComponent implements OnInit {
 
   artwork = [
     {
-      src: '/assets/images/biscuits.jpg',
+      src: '../../assets/images/biscuits.jpg',
       alt: 'Alfons Mucha - 1896 - Biscuits Lefèvre-Utile',
       title: 'Poster for Biscuits Lefèvre-Utile',
       year: '(1896)'
     },
     {
-      src: '/assets/images/champenois.jpg',
+      src: '../../assets/images/champenois.jpg',
       alt: 'Alfons Mucha - 1898 - Rêverie (F. Champenois Calendar 1898)',
       title: 'Rêverie (F. Champenois Calendar 1898)',
       year: '(1898)'
     },
     {
-      src: '/assets/images/bouquet.jpg',
+      src: '../../assets/images/bouquet.jpg',
       alt: 'Alfons Mucha - 1900 - The Seasons: Spring (detail)',
       title: 'The Seasons: Spring (detail)',
       year: '(1900)'
     },
     {
-      src: '/assets/images/four_seasons.jpg',
+      src: '../../assets/images/four_seasons.jpg',
       alt: 'Alfons Mucha - 1897 - The Seasons (detail)',
       title: 'The Seasons (detail)',
       year: '(1897)'
     },
     {
-      src: '/assets/images/trappistine.jpg',
+      src: '../../assets/images/trappistine.jpg',
       alt: 'Alfons Mucha - 1897 - La Trappistine (detail)',
       title: 'Poster for La Trappistine (detail)',
       year: '(1897)'
     },
     {
-      src: '/assets/images/champagne.jpg',
+      src: '../../assets/images/champagne.jpg',
       alt: 'Alfons Mucha - 1896 - Biscuits Champagne Lefèvre-Utile',
       title: 'Poster for Biscuits Champagne Lefèvre-Utile',
       year: '(1896)'
     },
     {
-      src: '/assets/images/fruit.jpg',
+      src: '../../assets/images/fruit.jpg',
       alt: 'Alfons Mucha - 1897 - Fruit',
       title: 'Fruit',
       year: '(1897)'
     },
     {
-      src: '/assets/images/autumn.jpg',
+      src: '../../assets/images/autumn.jpg',
       alt: 'Alfons Mucha - 1896 - The Seasons: Autumn',
       title: 'The Seasons: Autumn',
       year: '(1896)'
