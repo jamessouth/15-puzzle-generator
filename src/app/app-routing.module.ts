833292db1467b6d2f60e6af558ed714dd99a86da
jamessouth <jamesdanielsouth@gmail.com>
2019-11-04 00:57:43 -0600

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { DemoComponent } from './demo/demo.component';
 import { ArtworkComponent } from './artwork/artwork.component';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent, data: { animation: 'HomePage' } },
+    { path: '', component: HomeComponent, data: { animation: 'HomePage' } },
     { path: 'demo', component: DemoComponent, data: { animation: 'DemoPage' } },
     { path: 'artwork', component: ArtworkComponent, data: { animation: 'ArtworkPage' } },
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
