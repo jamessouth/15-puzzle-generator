ea0d27594510f6ec24073e9e20996f22ce19f88c
jamessouth <jamesdanielsouth@gmail.com>
2019-11-04 02:10:54 -0600

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,19 @@ import { DemoComponent } from './demo/demo.component';
 import { ArtworkComponent } from './artwork/artwork.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent, data: { animation: 'HomePage' } },
+    { path: 'home', component: HomeComponent, data: { animation: 'HomePage' } },
     { path: 'demo', component: DemoComponent, data: { animation: 'DemoPage' } },
     { path: 'artwork', component: ArtworkComponent, data: { animation: 'ArtworkPage' } },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
+
+// audit
+// const routes: Routes = [
+//     { path: '', component: HomeComponent, data: { animation: 'HomePage' } },
+//     { path: 'demo', component: DemoComponent, data: { animation: 'DemoPage' } },
+//     { path: 'artwork', component: ArtworkComponent, data: { animation: 'ArtworkPage' } },
+//     { path: '**', redirectTo: '', pathMatch: 'full' }
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
